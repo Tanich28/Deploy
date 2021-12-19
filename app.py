a89db5f817b42adda734b972af56a74e018ddfe1
Tanich28 <tatizinkevich@gmail.com>
2021-12-19 13:05:07 +0300

    deploy3

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import os
 
 import telebot
+from aiogram.utils import executor
 from flask import Flask, request
 
 from data.config import APP_URL, BOT_TOKEN
-from loader import bot
+from loader import bot, dp
 from utils.set_bot_commands import set_default_commands
 
 
 # bot = telebot.TeleBot(BOT_TOKEN)
-server = Flask(__name__)
+# server = Flask(__name__)
 async def on_startup(dp):
     import filters
     import middlewares
@@ -28,14 +29,17 @@ async def on_startup(dp):
 #
 #     executor.start_polling(dp, on_startup=on_startup)
 
-@server.route(f"/{BOT_TOKEN}", methods=["POST"])
-def redirect_message():
-    json_string = request.get_data().decode("utf-8")
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
+# @server.route(f"/{BOT_TOKEN}", methods=["POST"])
+# def redirect_message():
+#     json_string = request.get_data().decode("utf-8")
+#     update = telebot.types.Update.de_json(json_string)
+#     bot.process_new_updates([update])
+#     return "!", 200
+#
+# if __name__ == "__main__":
+#     bot.remove_webhook()
+#     bot.set_webhook(url=APP_URL)
+#     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    executor.start_polling(dp, skip_updates=True)
