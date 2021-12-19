52a62ac64dc02189c5d2abe5f0cd25cd9719a339
Tanich28 <tatizinkevich@gmail.com>
2021-12-19 12:29:27 +0300

    deploy2

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
 
-from flask import Flask
+import telebot
+from flask import Flask, request
 
-from data.config import APP_URL
+from data.config import APP_URL, BOT_TOKEN
 from loader import bot
 from utils.set_bot_commands import set_default_commands
 
+
+# bot = telebot.TeleBot(BOT_TOKEN)
 server = Flask(__name__)
 async def on_startup(dp):
     import filters
@@ -25,6 +28,12 @@ async def on_startup(dp):
 #
 #     executor.start_polling(dp, on_startup=on_startup)
 
+@server.route(f"/{BOT_TOKEN}", methods=["POST"])
+def redirect_message():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
 
 if __name__ == "__main__":
     bot.remove_webhook()
